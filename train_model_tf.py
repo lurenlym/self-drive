ed1a25fd39809d9ff1e94f6225b7d5bd7faebd01
lurenlym <lurenlym@163.com>
2017-10-08 16:51:58 +0800

    add train tf file

---
+++ train_model_tf.py
@@ -0,0 +1,107 @@
+__author__ = 'dl'
+
+import csv
+import cv2
+import numpy as np
+import random
+#import tensorflow as tf
+#from keras import optimizers
+#from keras.callbacks import ModelCheckpoint
+#from keras.models import Sequential
+#from keras.layers import Flatten, Dense, Dropout, Lambda
+#from keras.layers import Convolution2D, Cropping2D, MaxPooling2D
+
+
+def read_log(filepath):
+    lines = []
+    with open(filepath) as csvfile:
+        reader = csv.reader(csvfile)
+        for line in reader:
+            lines.append(line)
+
+    return lines
+
+
+# Read original data
+lines_twolaps = read_log('E:\\myproject\self-drive\IMG\driving_log.csv')
+lines_curves = read_log('E:\\myproject\self-drive\IMG\driving_log.csv')
+lines = np.array(lines_twolaps + lines_curves)
+
+# Balance data
+nbins = 2000
+max_examples = 200
+balanced = np.empty([0, lines.shape[1]], dtype=lines.dtype)  
+for i in range(0, nbins):
+    begin = i * (1.0 / nbins)
+    end = begin + 1.0 / nbins
+    extracted = lines[(abs(lines[:,3].astype(float)) >= begin) & (abs(lines[:,3].astype(float)) < end)]
+    np.random.shuffle(extracted)
+    extracted = extracted[0:max_examples, :]
+    balanced = np.concatenate((balanced, extracted), axis=0)
+
+# Prepare and augment data
+imgs, angles = [], []
+offset = 0.2
+correction = [0, offset, -offset]  # center, left, right cameras
+for line in balanced:
+    for i in range(3):
+        img_path = line[i]
+        img = cv2.imread(img_path)
+        imgs.append(img)
+
+        angle = float(line[3])
+        angles.append(angle + correction[i])
+
+flip_imgs, flip_angles = [], []
+for img, angle in zip(imgs, angles):
+    flip_imgs.append(cv2.flip(img, 1))
+    flip_angles.append(-1.0 * angle)
+
+augmented_imgs = imgs + flip_imgs
+augmented_angles = angles + flip_angles
+
+X_test,Y_test = [],[]
+random_index = list(range(len(augmented_angles)))
+for i in range(int(len(augmented_angles)*0.1)):
+    random_index = list(range(len(augmented_angles)))
+    a = random.choice(random_index)
+    X_test.append(augmented_imgs[a])
+    Y_test.append(augmented_angles[a])
+    augmented_angles.pop(a)
+    augmented_imgs.pop(a)
+    random_index.pop(a)
+    
+X_train = np.array(augmented_imgs)
+Y_train = np.array(augmented_angles)
+
+X_test = np.array(X_test)
+Y_test = np.array(Y_test)
+
+
+
+# Build the model
+model = Sequential()
+model.add(Lambda(lambda x: x/255.0-0.5, input_shape=(160,320,3)))
+model.add(Cropping2D(cropping=((70,25), (0,0))))
+model.add(Convolution2D(8, 3, 3, activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.2))
+model.add(Convolution2D(16, 3, 3, activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.2))
+model.add(Convolution2D(32, 3, 3, activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.2))
+model.add(Flatten())
+model.add(Dense(512, activation='relu'))
+model.add(Dropout(0.2))
+model.add(Dense(256, activation='relu'))
+model.add(Dropout(0.2))
+model.add(Dense(1))
+
+model.compile(loss='mse', optimizer=optimizers.Adam(lr=0.0001), metrics=['accuracy'])
+tbCallBack = keras.callbacks.TensorBoard(log_dir='./logs', histogram_freq=0, write_graph=True, write_images=True,)
+best_model = ModelCheckpoint('model_best.h5', verbose=2, save_best_only=True)
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=30, callbacks=[best_model,tbCallBack])
+
+model.save('model_last.h5')
